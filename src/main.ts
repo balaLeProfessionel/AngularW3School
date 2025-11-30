231556e5c39d4c861d285cb6fc739fecd2f9da36
BARRY <bala.professionnel01@gmail.com>
2025-11-30 08:30:17 +0000

    Les variables de reférences

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+@Component({   //component est une classe qui controle une vue
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  // styles: [`
+  // .toobar {display:flex; gap:}
+  //   `]
+  template: `
+    <h3>Template Reference Variables (#var)</h3>
+    <div class="toolbar">
+    <input #box type="text" placeholder="Type something" (input)="current= box.value"/>
+    <button (click)="read(box.value)">Read Value</button>
+    <button (click)="box.focus()">Focus input</button>
+    <span style="margin-left:8px; color:#666">lenght={{box.value?.length || 0 }} </span>
+    </div>
+    <p>Current:{{current || ('empty')}} </p>
+  `
+})
+
+export class App {
+  current = 'Baba Ndiaye';
+  read(val:string) {this.current = val ?? ';'}
+}
+
+bootstrapApplication(App);
