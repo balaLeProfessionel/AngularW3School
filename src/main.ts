173efb5adefdd066e767d1a5196bcdeb625127eb
BARRY <bala.professionnel01@gmail.com>
2025-11-30 09:36:01 +0000

    Navigation securise en cas de valeur null

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // Necessaire pour utiliser #variableDeReference
+import { email } from '@angular/forms/signals';
 
 @Component({   //component est une classe qui controle une vue
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  // styles: [`
-  // .toobar {display:flex; gap:}
-  //   `]
   template: `
-    <h3>Template Reference Variables (#var)</h3>
-    <div class="toolbar">
-    <input #box type="text" placeholder="Type something" (input)="current= box.value"/>
-    <button (click)="read(box.value)">Read Value</button>
-    <button (click)="box.focus()">Focus input</button>
-    <span style="margin-left:8px; color:#666">lenght={{box.value?.length || 0 }} </span>
-    </div>
-    <p>Current:{{current || ('empty')}} </p>
+    <button (click)="toggle()">Toggle user</button>
+    <p>Email: {{ user?.profile?.email || '(none)' }} </p>
   `
 })
 
 export class App {
-  current = 'Baba Ndiaye';
-  read(val:string) {this.current = val ?? ';'}
+  user: { profile?: { email?: string}  } | undefined = undefined;
+  toggle() {
+    this.user = this.user ? undefined : { profile: {email: 'ept@empty.sn' } };
 }
-
+}
 bootstrapApplication(App);
